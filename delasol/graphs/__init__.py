06d12824169827d7b74950e399b8f6d86c2bf7c6
bacor <accounts@bascornelissen.nl>
2025-01-17 00:00:47 +0100

    add some apis

--- delasol/graphs/__init__.py
+++ delasol/graphs/__init__.py
@@ -7,4 +7,3 @@
 from delasol.graphs.hexachord_graph import HexachordGraph
 from delasol.graphs.gamut_graph import GamutGraph
 from delasol.graphs.rollout_graph import RolloutGraph
-from backup.solmization_graph_v2 import SolmizationGraph
