d2a3e1e11607d4a785ab4a235368ab7e71b23a02
bacor <accounts@bascornelissen.nl>
2025-01-17 11:51:34 +0100

    update test

--- tests/test_continental_style.py
+++ tests/test_continental_style.py
@@ -7,20 +7,26 @@ import unittest
 from music21.pitch import Pitch
 
 # Local imports
-from delasol.graphs.hexachord_graph import Hexachord
+from delasol.graphs.hexachord_graph import HexachordGraph
 from delasol.graphs.gamut_graph import GamutGraph as Gamut
-from delasol.styles.continental_16c import CONTINENTAL_MUTATIONS
-from delasol.styles import HardContinental16CenturyGamut as HardGamut
-from delasol.styles import SoftContinental16CenturyGamut as SoftGamut
+from delasol.solmizers.continental_16c import CONTINENTAL_MUTATIONS
+from delasol.solmizers.continental_16c import (
+    HardContinental16CenturyGamutGraph as HardGamut,
+)
+from delasol.solmizers.continental_16c import (
+    SoftContinental16CenturyGamutGraph as SoftGamut,
+)
 
 
 class TestHardContinentalGamut(unittest.TestCase):
     def test_CONTINENTAL_MUTATIONS(self):
-        H1 = Hexachord("G2")
-        H2 = Hexachord("C3")
+        H1 = HexachordGraph("G2")
+        H2 = HexachordGraph("C3")
         G = Gamut(hexachords=[H1, H2], mutations=CONTINENTAL_MUTATIONS)
-        self.assertTrue((G.names["fa_G2"], G.names["re_C3"]) in G.edges)
-        self.assertTrue((G.names["fa_C3"], G.names["la_G2"]) in G.edges)
+        self.assertTrue((G.get_node(name="fa_G2"), G.get_node(name="re_C3")) in G.edges)
+        self.assertTrue(
+            (G.get_node(name="fa_C3"), G.names.get_node(name="la_G2")) in G.edges
+        )
 
     def test_hard_gamut(self):
         gamut = HardGamut()
