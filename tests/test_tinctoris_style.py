31c756e71217dc25029b96653ad1f00b6f5f8699
Artur J. Dobija <arturdobija@gmail.com>
2025-06-17 19:12:56 +0200

    Added to dos.

--- tests/test_tinctoris_style.py
+++ tests/test_tinctoris_style.py
@@ -4,6 +4,8 @@ from delasol.corpus import Corpus
 
 import matplotlib.pyplot as plt
 
+# To do: make it a real test.
+
 score = m21.converter.parse('tests/scores/MdB001.musicxml')
 solfa = solmize(score, style="tinctoris_15c")
 solfa.annotate(format="mutation")
